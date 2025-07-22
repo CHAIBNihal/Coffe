3e47ce1020d94c4970570f46d194068cee39c305
NIHAL <nihalchaib20@gmail.com>
2025-07-22 14:40:31 +0100

    second coffe comit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
-
-
-import './index.css'
+import { Routes, Route } from 'react-router-dom'
 import Nav from './components/NavBar/Nav'
 import HeroSection from './components/HeroSection/HeroSection'
-
 import Menu from './components/MenuCoffe/Menu'
 import TestimonialSlider from './components/Testemonie/Testemonie'
 import ContactUs from './components/ContactUs/ContactUs'
+import Home from './Pages/Home'
 
-function App() {
-  
-
+function HomePage() {
   return (
     <>
-      
-        <Nav/>
-        <HeroSection/>
-        <Menu />
-        <TestimonialSlider/>
-        <ContactUs />
-        
-      
+      <HeroSection />
+      <Menu />
+      <TestimonialSlider />
+      <ContactUs />
+    </>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<Home />} />
+      </Routes>
     </>
   )
 }
